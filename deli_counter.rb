c1d05a68ed26d241673ca94e68a0c1d1ad3c1330
Morgan Fudge <mfudge92@gmail.com>
2017-10-31 11:48:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,11 @@ def line(arr)
     puts "The line is currently empty."
     return
   end
-
   str = "The line is currently:"
   arr.each_with_index do |name, index|
     str << " #{index + 1}. #{name}"
   end
-
   puts str
-
 end
 
 def now_serving(arr)
