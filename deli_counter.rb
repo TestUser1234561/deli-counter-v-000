a6f0ed185179ffb09b0f743aae7fe9a4455927d5
Morgan Fudge <mfudge92@gmail.com>
2017-10-31 11:38:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def nowServing(arr) {
+  if(arr.size == 0) {
+    return "There is nobody waiting to be served!"
+  }
+
+  let cs = `Currently serving ${arr[0]}.`
+  arr.shift()
+  return cs
+}
