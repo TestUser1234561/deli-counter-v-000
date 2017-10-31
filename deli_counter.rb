ef27e2007a79442f59d4ad0b6be63b9aee1111aa
Morgan Fudge <mfudge92@gmail.com>
2017-10-31 11:52:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ end
 
 def now_serving(arr)
   if(arr.size == 0)
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   end
 
   let cs = "Currently serving #{arr[0]}."
